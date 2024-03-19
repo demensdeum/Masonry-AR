2a84b6d895d8e5979ca860474c19f7a30bd6f5a4
Ilia Prokhorov <demensdeum@gmail.com>
2024-03-20 01:21:48 +0300

    SQL scripts

--- server/sql/entities.sql
+++ server/sql/entities.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 04, 2024 at 01:15 AM
+-- Generation Time: Mar 19, 2024 at 10:17 PM
 -- Server version: 11.3.2-MariaDB
--- PHP Version: 8.3.3
+-- PHP Version: 8.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `masonry_ar`
 --
-CREATE DATABASE IF NOT EXISTS `demens_masonry_ar` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `demens_masonry_ar`;
 
 -- --------------------------------------------------------
 
@@ -41,28 +39,9 @@ CREATE TABLE `entities` (
   `balance` int(11) NOT NULL DEFAULT 0,
   `latitude` float NOT NULL,
   `longitude` float NOT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `creation_date` datetime NOT NULL DEFAULT utc_timestamp(),
+  `update_date` datetime NOT NULL DEFAULT utc_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `info`
---
-
-CREATE TABLE `info` (
-  `key` varchar(32) NOT NULL DEFAULT 'NONE',
-  `value` varchar(256) NOT NULL DEFAULT 'NONE'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `info`
---
-
-INSERT INTO `info` (`key`, `value`) VALUES
-('minimal_client_version', '4'),
-('scheme_version', '4');
 
 --
 -- Indexes for dumped tables
@@ -76,13 +55,6 @@ ALTER TABLE `entities`
   ADD UNIQUE KEY `uuid` (`uuid`);
 
 --
--- Indexes for table `info`
---
-ALTER TABLE `info`
-  ADD PRIMARY KEY (`key`),
-  ADD UNIQUE KEY `key` (`key`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -90,5 +62,9 @@ ALTER TABLE `info`
 -- AUTO_INCREMENT for table `entities`
 --
 ALTER TABLE `entities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1848;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1999;
+COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
