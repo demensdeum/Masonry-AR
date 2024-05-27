aa70ae27f47e228145708d48507280679ab8e79e
Ilia Prokhorov <demensdeum@gmail.com>
2024-05-27 21:15:39 +0400

    Hero for user protocol altered

--- server/src/heroForUser.php
+++ server/src/heroForUser.php
@@ -17,8 +17,14 @@ $stmt->bind_param("s", $session_uuid);
 $stmt->execute();
 
 $result = $stmt->get_result();
-$entities = $result->fetch_all(MYSQLI_ASSOC);
+$hero = $result->fetch_assoc();
 
-echo json_encode(array('code' => 0, 'entities' => $entities), JSON_UNESCAPED_UNICODE);
-
-$conn->close();
+if ($hero) {
+    echo json_encode(array('code' => 0, 'message' => "Hero exists!", 'hero' => $hero), JSON_UNESCAPED_UNICODE);
+    $conn->close();
+    exit(0);
+} else {
+    echo json_encode(array('code' => 1, 'message' => "No hero", 'hero' => NULL), JSON_UNESCAPED_UNICODE);
+    $conn->close();
+    exit(0);
+}
